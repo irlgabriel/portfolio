f8e6aba212c9b68ce08e1f70908f4d85705bbd0d
irlgabriel <radu.gabriel131@gmail.com>
2020-10-04 01:06:59 +0300

    made footer responsive and added About Me content

--- src/components/About/About.components.js
+++ src/components/About/About.components.js
@@ -1,11 +1,39 @@
 import styled from "styled-components"
 
 export const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  
+  padding: 1rem;
+  margin-bottom: auto;
+  min-height: 70vh;
 `
 
 export const Heading = styled.h1`
+  width: 100%;
+`
 
+export const InfoColumn = styled.section`
+  margin-top: 1.5rem;
+  margin-right: auto;
+  padding: 2rem .5rem;
+  max-width: 60%;
+  background: whitesmoke;
+  border-radius: 5px;
+
+  &:nth-child(odd) {
+    margin-right: 0;
+    margin-left: auto;
+  }
+
+  @media screen and (max-width: 768px) {
+    margin: 1.5rem 0 0 0;
+    max-width: 100%;
+
+  }
+`
+
+export const Content = styled.div`
+
+`
+
+export const Para = styled.p`
+  margin-top: .5rem;
 `
--- src/components/Footer/Footer.components.js
+++ src/components/Footer/Footer.components.js
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 
 export const MainContainer = styled.section`
+  padding: 1.5rem;
   border-top: 1px solid white;
   color: white;
   min-height: 100px;
@@ -9,10 +10,17 @@ export const MainContainer = styled.section`
   background: #101522;
   justify-content: space-around;
   align-items: center;
+
+  @media screen and (max-width:960px) {
+    flex-direction: column;
+  }
 `
 
 export const ContactInfo = styled.div`
  
+  @media screen and (max-width: 960px) {
+    margin-bottom: 1rem;
+  }
 `
 export const TopLine = styled.div`
   display: flex;

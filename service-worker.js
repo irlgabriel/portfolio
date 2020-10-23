d45fb7737c904ad04c171ecce73c3bb701d12548
irlgabriel <radu.gabriel131@gmail.com>
2020-10-23 20:27:17 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.8217c075636aad9e0e15a1204e4ed663.js"
+  "/portfolio/precache-manifest.d71ec3032edf43c05c15f161d65dfec5.js"
 );
 
 self.addEventListener('message', (event) => {
